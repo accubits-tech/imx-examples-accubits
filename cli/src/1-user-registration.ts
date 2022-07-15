8b845e8fa078c471a7dd23a417f924b5e6c16ca2
mayanksharma-ab <mayank.sharma@accubits.com>
2022-07-15 10:12:51 +0530

    mint command fixes, code formating

--- cli/src/1-user-registration.ts
+++ cli/src/1-user-registration.ts
@@ -10,47 +10,53 @@ import {
   UsersApi,
 } from '@imtbl/core-sdk';
 
-
-export default async (network:EthNetwork,privateKey:string,alchemyApiKey:string): Promise<void> => {
+export default async (
+  network: EthNetwork,
+  privateKey: string,
+  alchemyApiKey: string,
+): Promise<void> => {
   const config = getConfig(network);
   const usersApi = new UsersApi(config.api);
-  const provider = new AlchemyProvider(network,alchemyApiKey);
-  const user = new Wallet(privateKey).connect(provider)
+  const provider = new AlchemyProvider(network, alchemyApiKey);
+  const user = new Wallet(privateKey).connect(provider);
   let existingUser;
   let newUser;
   try {
     // Fetching existing user
-    const getUserRes = await usersApi.getUsers({user:user.address})
-    existingUser=getUserRes?.data||{};
+    const getUserRes = await usersApi.getUsers({ user: user.address });
+    existingUser = getUserRes?.data || {};
   } catch {
     try {
-    const userStarkWallet = await generateStarkWallet(user);
-     // Get signable details for offchain registration
-    const userSignableResult = await usersApi.getSignableRegistrationOffchain({
-    getSignableRegistrationRequest: {
-      ether_key: user.address,
-      stark_key: userStarkWallet.starkPublicKey,
-
-    },
-  });
-  const { signable_message: userSignableMessage, payload_hash: userPayloadHash } =
-  userSignableResult.data;
-  // Sign message with L1 credentials 
-  const userEthSignature = await signRaw(userSignableMessage, user);
-  // Sign hash with L2 credentials
-  const userStarkSignature = serializeSignature(
-    sign(userStarkWallet.starkKeyPair, userPayloadHash),
-  );
-  const registerUserRes = await usersApi.registerUser({
-    registerUserRequest: {
-      eth_signature: userEthSignature,
-      ether_key: user.address,
-      stark_signature: userStarkSignature,
-      stark_key: userStarkWallet.starkPublicKey,
-    },
-  });
-  newUser=registerUserRes?.data||{}
-  }  catch (error) {
+      const userStarkWallet = await generateStarkWallet(user);
+      // Get signable details for offchain registration
+      const userSignableResult = await usersApi.getSignableRegistrationOffchain(
+        {
+          getSignableRegistrationRequest: {
+            ether_key: user.address,
+            stark_key: userStarkWallet.starkPublicKey,
+          },
+        },
+      );
+      const {
+        signable_message: userSignableMessage,
+        payload_hash: userPayloadHash,
+      } = userSignableResult.data;
+      // Sign message with L1 credentials
+      const userEthSignature = await signRaw(userSignableMessage, user);
+      // Sign hash with L2 credentials
+      const userStarkSignature = serializeSignature(
+        sign(userStarkWallet.starkKeyPair, userPayloadHash),
+      );
+      const registerUserRes = await usersApi.registerUser({
+        registerUserRequest: {
+          eth_signature: userEthSignature,
+          ether_key: user.address,
+          stark_signature: userStarkSignature,
+          stark_key: userStarkWallet.starkPublicKey,
+        },
+      });
+      newUser = registerUserRes?.data || {};
+    } catch (error) {
       throw new Error(JSON.stringify(error, null, 2));
     }
   }
@@ -60,5 +66,4 @@ export default async (network:EthNetwork,privateKey:string,alchemyApiKey:string)
     console.log('User has been created', user.address);
   }
   console.log(JSON.stringify({ newUser, existingUser }, null, 2));
-
-}
+};
--- cli/src/3-create-collection.ts
+++ cli/src/3-create-collection.ts
@@ -8,32 +8,41 @@ import {
   signRaw,
 } from '@imtbl/core-sdk';
 
-export default async (network:EthNetwork,privateKey:string ,alchemyApiKey:string, contractAddress:string,name:string,ownerPublicKey:string, projectId:number ) : Promise<void> => {
+export default async (
+  network: EthNetwork,
+  privateKey: string,
+  alchemyApiKey: string,
+  contractAddress: string,
+  name: string,
+  ownerPublicKey: string,
+  projectId: number,
+): Promise<void> => {
   try {
-  const config = getConfig(network);
-  const provider = new AlchemyProvider(network, alchemyApiKey);
-  const user = new Wallet(privateKey).connect(provider)
-  //generate authorisation headers
-  const timestamp = Math.floor(Date.now() / 1000).toString();
-  const signature = await signRaw(timestamp, user);
-  //Create a new project
-  const collectionsApi = new CollectionsApi(config.api);
-  const createCollectionParams: CollectionsApiCreateCollectionRequest={
-    createCollectionRequest: {
-      'contract_address': contractAddress,
-       'name': name,
-       'owner_public_key':ownerPublicKey,
-       "project_id":projectId
-    },
-    iMXSignature: signature,
-    iMXTimestamp: timestamp,
+    const config = getConfig(network);
+    const provider = new AlchemyProvider(network, alchemyApiKey);
+    const user = new Wallet(privateKey).connect(provider);
+    //generate authorisation headers
+    const timestamp = Math.floor(Date.now() / 1000).toString();
+    const signature = await signRaw(timestamp, user);
+    //Create a new project
+    const collectionsApi = new CollectionsApi(config.api);
+    const createCollectionParams: CollectionsApiCreateCollectionRequest = {
+      createCollectionRequest: {
+        contract_address: contractAddress,
+        name: name,
+        owner_public_key: ownerPublicKey,
+        project_id: projectId,
+      },
+      iMXSignature: signature,
+      iMXTimestamp: timestamp,
+    };
+    console.log(createCollectionParams);
+    const createCollectionRes = await collectionsApi.createCollection(
+      createCollectionParams,
+    );
+    const collection = createCollectionRes?.data;
+    console.log('Created collection', collection);
+  } catch (err) {
+    console.log(err);
   }
-  console.log(createCollectionParams)
-  const createCollectionRes = await collectionsApi.createCollection(createCollectionParams)
-  const collection = createCollectionRes?.data ;
-  console.log('Created collection', collection);
-  } catch (err){
-    console.log(err)
-   
-  }
-}
+};
